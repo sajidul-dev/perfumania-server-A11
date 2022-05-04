79ada5598f3e07bfaed6a1e49385f81995172882
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-05 01:20:15 +0600

    Server file created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Runnging warehouse server')
+})
+
+app.listen(port, () => {
+    console.log("Listening", port);
+})
