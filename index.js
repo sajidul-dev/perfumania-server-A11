8893fad2518a2b996dbea4918d7c79fc7a49e23d
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-07 21:43:20 +0600

    Delete operation updated

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function run() {
         app.delete('/item/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
-            const result = await addedCollection.deleteOne(query)
+            const result = await perfumeCollection.deleteOne(query)
             res.send(result)
         })
 
